5607b32b7ff7ae5ffa949fb5af25e6a9e6825b23
Linde Vloeberghs <lindevloeberghs@me.com>
2017-03-30 10:12:25 +0200

    configure postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "https://mailing-demo-62.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "https://mailing-demo-62.herokuapp.com" }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
