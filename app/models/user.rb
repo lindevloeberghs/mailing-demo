78d38b9173d1410da3bf2545e0db74d4ec2e409f
Linde Vloeberghs <lindevloeberghs@me.com>
2017-03-30 10:36:17 +0200

    add users to mailchimp list when user is created (gibbon gem)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   after_create :send_welcome_email
+  after_create :subscribe_to_newsletter
 
   private
 
   def send_welcome_email
     UserMailer.welcome(self).deliver_now
+  end
+
+  def subscribe_to_newsletter
+    SubscribeToNewsletterService.new(self).call
   end
 
 end
